4cadeda807f9252ad08fd5d02df786f86bf4db1a
mthom26 <mthom2626@gmail.com>
2019-11-27 22:30:47 +0000

    Add `spheres` scene

--- src/scene.rs
+++ src/scene.rs
@@ -4,12 +4,14 @@ use crate::{
     camera::Camera,
     material::{Dielectric, Lambertian, Metal},
     shapes::{Hittable, Sphere},
+    utils::gen_random,
     vector::Vec3,
 };
 
 pub fn load_scene(scene_name: String, x: u64, y: u64) -> (Camera, Vec<Box<dyn Hittable>>) {
     match &scene_name[..] {
         "default" => default_scene(x, y),
+        "spheres" => spheres_scene(x, y),
         _ => panic!("Could not load scene."),
     }
 }
@@ -34,6 +36,100 @@ fn default_scene(x: u64, y: u64) -> (Camera, Vec<Box<dyn Hittable>>) {
     let to = Vec3::new(0.0, 0.0, -1.0);
     let up = Vec3::new(0.0, 1.0, 0.0);
     let aperture = 0.5;
+    let focus_dist = (from - to).get_mag();
+    let cam = Camera::new(from, to, up, 50.0, aspect_ratio, aperture, focus_dist);
+
+    (cam, world)
+}
+
+fn spheres_scene(x: u64, y: u64) -> (Camera, Vec<Box<dyn Hittable>>) {
+    let ground_mat = Arc::new(Lambertian::new(Vec3::new(0.8, 0.3, 0.2)));
+    let mut world: Vec<Box<dyn Hittable>> = vec![Box::new(Sphere::new(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_mat,
+    ))];
+
+    let collisions = vec![
+        (Vec3::new(-3.0, 0.6, 1.5), 0.6),
+        (Vec3::new(-4.0, 1.4, -2.0), 1.4),
+        (Vec3::new(2.0, 0.5, -2.0), 0.5),
+    ];
+
+    let mat_one = Arc::new(Metal::new(Vec3::new(0.3, 0.2, 0.8), 0.05));
+    let mat_two = Arc::new(Metal::new(Vec3::new(0.6, 0.9, 0.6), 0.2));
+    let mat_three = Arc::new(Dielectric::new(1.5));
+    let sphere_one = Box::new(Sphere::new(collisions[0].0, collisions[0].1, mat_one));
+    let big_boi = Box::new(Sphere::new(collisions[1].0, collisions[1].1, mat_two));
+    let sphere_three = Box::new(Sphere::new(collisions[2].0, collisions[2].1, mat_three));
+    world.push(sphere_one);
+    world.push(big_boi);
+    world.push(sphere_three);
+
+    // Convenience function to make sure a new postion and radius won't collide
+    // with the given Vec of spheres
+    fn check_for_collision(center: Vec3, radius: f32, collisions: &Vec<(Vec3, f32)>) -> bool {
+        for sphere in collisions.into_iter() {
+            let a = (sphere.0 - center).get_mag();
+            let b = sphere.1 + radius;
+            if a < b {
+                return true;
+            }
+        }
+        false
+    }
+
+    for x in -11..11 {
+        for z in -11..11 {
+            let radius = gen_random() / 5.0;
+            let mat_chance = gen_random();
+
+            let center = Vec3::new(
+                x as f32 + 0.25 + gen_random() / 2.0,
+                radius,
+                z as f32 + 0.25 + gen_random() / 2.0,
+            );
+
+            if check_for_collision(center, radius, &collisions) {
+                continue;
+            }
+            if gen_random() < 0.8 {
+                match mat_chance {
+                    val if val < 0.6 => {
+                        // Diffuse
+                        let mat = Arc::new(Lambertian::new(Vec3::new(
+                            gen_random(),
+                            gen_random(),
+                            gen_random(),
+                        )));
+                        let sphere = Box::new(Sphere::new(center, radius, mat));
+                        world.push(sphere);
+                    }
+                    val if val < 0.85 => {
+                        // Metallic
+                        let mat = Arc::new(Metal::new(
+                            Vec3::new(gen_random(), gen_random(), gen_random()),
+                            gen_random(),
+                        ));
+                        let sphere = Box::new(Sphere::new(center, radius, mat));
+                        world.push(sphere);
+                    }
+                    _ => {
+                        // Glass
+                        let mat = Arc::new(Dielectric::new(gen_random()));
+                        let sphere = Box::new(Sphere::new(center, radius, mat));
+                        world.push(sphere);
+                    }
+                }
+            }
+        }
+    }
+
+    let aspect_ratio = x as f32 / y as f32;
+    let from = Vec3::new(3.0, 1.5, 2.0);
+    let to = Vec3::new(0.0, 0.0, -1.0);
+    let up = Vec3::new(0.0, 1.0, 0.0);
+    let aperture = 0.05;
     let focus_dist = (from - to).get_mag();
     let cam = Camera::new(from, to, up, 50.0, aspect_ratio, aperture, focus_dist);
 
